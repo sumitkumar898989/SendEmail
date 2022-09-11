4e359401bb09aa3036395c3bffa19fc5aec1dff9
sumitkumar898989 <113261917+sumitkumar898989@users.noreply.github.com>
2022-09-11 20:44:12 +0530

    Commit Final

--- SendEmail/src/main/java/com/example/demo/SendEmail.java
+++ SendEmail/src/main/java/com/example/demo/SendEmail.java
@@ -13,6 +13,7 @@ public class SendEmail {
 	public void sendEmail() {
 		  System.out.println("in sendEmail method");
 		  SimpleMailMessage msg = new SimpleMailMessage(); msg.setTo("shiveshkumar9507575556@gmail.com");
+		  //sumit
 		  //SimpleMailMessage msg = new SimpleMailMessage(); msg.setTo("sumit.z.kumar@capgemini.com","venkata-naga-sai-hari-krishna.metlapalli@capgemini.com","ankit.c.singhal@capgemini.com","avinash.y.kumar@capgemini.com");
 		  msg.setSubject("Order Status Report");
 		  msg.setText("Hello \n Test Email");
